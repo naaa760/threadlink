21116fd82491fac76359abeef7253817adb52206
nehhhhha6 <n0370525@gmail.com>
2025-01-02 06:18:08 +0530

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import ModeToggle from "@/components/ModeToggle";
+
+export default function Home() {
+  return (
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign-in</Button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
+      <ModeToggle />
+      <Button variant={"secondary"}>Click-me</Button>
+    </div>
+  );
+}
